fd051a87ecf48b9658ea8563fc46323404b0c1c2
jayta <jayta@Jay>
2023-03-01 16:15:59 +0530

    MyFirstCommit.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jayta
+ *
+ */
+module ProjectDemo {
+}
---
+++ src/project/MyProject.java
@@ -0,0 +1,11 @@
+package project;
+
+public class MyProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello...");
+
+	}
+
+}
